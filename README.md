c8efcee37a647755fc9323cc940dc65c517def2c
detanx <detanxit@163.com>
2018-09-06 16:10:17 +0800

    update

--- README.md
+++ README.md
@@ -3,12 +3,15 @@
 ### 使用
 ####  1、下载安装
 ```javascript
-在项目中首先安装node环境
-检测是否安装node环境：win + r 输入cmd，在窗口中再输入node -v
-在项目中安装：npm init 
-              npm install --save-dev prototype-add-method
-使用：import 'prototype-add-method'
-      require('prototype-add-method')
+1、下载：https://github.com/detanx/prototype-add-method
+2、git获取：git clone git@github.com:detanx/prototype-add-method.git
+3、npm下载：
+  在项目中首先安装node环境
+  检测是否安装node环境：win + r 输入cmd，在窗口中再输入node -v
+  在项目中安装：npm init 
+                npm install --save-dev prototype-add-method
+  使用：import 'prototype-add-method'
+        require('prototype-add-method')
 ```
 ### 方法api列表
 #### 1、globalReplace
@@ -25,6 +28,39 @@ newStr：替换成新的字段
 str.findAllPosition(subStr)
 str：目标字符串
 subStr：查找的字符串
+```
+#### 3、findAllOccurrences
+```
+//查找数组中某个字符的所有位置
+arr.findAllOccurrences(target)
+arr：目标数组
+subStr：查找的元素
+```
+#### 4、uniqueElement
+```
+//数组去重
+arr.uniqueElement()
+arr：目标数组
+```
+#### 5、arraySum
+```
+//数组求和，当数组中传入null，undefined，NaN，字符串等非数字会被忽略
+arr.arraySum()
+arr：目标数组
+```
+#### 6、removeItem
+```
+//数组移除元素item（包括NaN、undefined、null）
+arr.removeItem(target)
+arr：目标数组
+target：需要移除的元素
+```
+#### 3、findAllOccurrences
+```
+//查找数组中某个字符的所有位置
+arr.findAllOccurrences(target)
+str：目标数组
+subStr：查找的元素
 ```
 #### 3、findAllOccurrences
 ```
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //将某个字符串中的某个字段全部替换为新的字段
 String.prototype.globalReplace =
-String.prototype.globalReplace ||function(oldStr, newStr){//oldStr替换成newStr 
-  let reg = new RegExp(oldStr, "g"); //创建正则RegExp对象 
+String.prototype.globalReplace ||function(oldStr, newStr, isIgnoreCase){//oldStr替换成newStr 
+  isIgnoreCase = typeof isIgnoreCase === 'boolean' ? "gi" : "g";
+  let reg = new RegExp(oldStr, isIgnoreCase); //创建正则RegExp对象 
   return this.replace(reg, newStr); 
 }
 
-//查找字符串中某个字符串的起始位置
+//查找字符串中某个字符串的所有起始位置
 String.prototype.findAllPosition = 
 String.prototype.findAllPosition || function(subStr) {
   let arr = [];
@@ -21,8 +22,15 @@ String.prototype.findAllPosition || function(subStr) {
 Array.prototype.findAllOccurrences = 
 Array.prototype.findAllOccurrences || function (target) {
   var result = [];
+  for(let i = 0;i < this.length; i ++) {
+    if(this[i] instanceof Array) {
+      this[i].filter(function(item,index) {
+        return item === target && result.push([i,index]);
+      });
+    }
+  } 
   this.filter(function(item,index) {
-      return item === target && result.push(index);
+    return item === target && result.push(index);
   });
   return result;
 }
@@ -30,33 +38,55 @@ Array.prototype.findAllOccurrences || function (target) {
 //数组去重
 Array.prototype.uniqueElement = 
 Array.prototype.uniqueElement || function() {
-  return Array.from(new Set(this))
+  //判断是否支持[native code]
+  function isNative(api){
+    return /native code/.test(api.toString())&&typeof api !== 'undefined'
+  }
+  if(isNative(Set)) {
+    return Array.from(new Set(this))
+  }
+  let newarr = [];
+  let len = this.length;
+  for(let i = 0 ; i < len ; i ++ ) {
+    if(newarr.indexOf(this[i]) === -1) {
+      newarr.push(this[i])
+    }
+  }
+  return newarr
 }
 
 //数组求和
 Array.prototype.arraySum = 
 Array.prototype.arraySum || function() {
-  var suma = 0;
+  var sum = 0;
   this.forEach( (item) => 
-    suma += item
-  });
-  return suma;
+    {
+      if(typeof item === 'number' && !isNaN(item)) {
+        sum += item
+      }
+    }
+  );
+  return sum;
 }
 
 //数组移除元素item
 Array.prototype.removeItem = 
 Array.prototype.removeItem || function(item){
-    return arr.filter(function(ele){
-         return ele != item;
-    })
+  return this.filter(function(ele){
+    if(isNaN(item)) {
+      return isNaN(ele) != isNaN(item);
+    }else {
+      return ele !== item;
+    }
+  })
 }
 
-//数组移指定位置indexc处添加元素item
+//数组移指定位置index处添加元素item
 Array.prototype.addItem = 
 Array.prototype.addItem || function(item, index) {
-    var s1 = this.slice(0,index)
-    var s2 = this.slice(index)
-    return s1.concat([item],s2)
+  var s1 = this.slice(0,index)
+  var s2 = this.slice(index)
+  return s1.concat([item],s2)
 }
 
 //数组重复出现的元素
