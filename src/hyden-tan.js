66421b3e638869b6ba057d1a6bfc3ed172d81d63
hyden <hydentan@foxmail.com>
2018-09-20 00:08:27 +0800

    add segArray

--- src/hyden-tan.js
+++ src/hyden-tan.js
@@ -1,1 +1,24 @@
-console.log('hyden-tan')
+/**
+ * @description 数组分段, 用法: segArray([1, 2, 3, 4], 2) // [[1, 2], [3, 4]]
+ * @param {*} arr 原数组
+ * @param {*} size 分段大小
+ */
+const segArray = (arr, size = 2) => arr.reduce((res, item, index) => {
+    if (!Array.isArray(arr)) {
+        return [];
+    }
+
+    if (size < 2) {
+        return arr;
+    }
+
+    if (index % size === 0) {
+        res.push(arr.slice(index, index + size));
+    }
+
+    return res;
+}, []);
+
+module.exports = {
+    segArray
+}
