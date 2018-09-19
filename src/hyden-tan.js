f5bbec68142a3e8a4bd0cd71f30d0d27614ea7e2
hyden <hydentan@foxmail.com>
2018-09-20 00:18:33 +0800

    fix type declare

--- src/hyden-tan.js
+++ src/hyden-tan.js
@@ -1,7 +1,7 @@
 /**
  * @description 数组分段, 用法: segArray([1, 2, 3, 4], 2) // [[1, 2], [3, 4]]
- * @param {*} arr 原数组
- * @param {*} size 分段大小
+ * @param {Array} arr 原数组
+ * @param {Integer} size 分段大小
  */
 const segArray = (arr, size = 2) => arr.reduce((res, item, index) => {
     if (!Array.isArray(arr)) {
